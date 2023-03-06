80c9f453c7e26ded197f37ad7f3ebfd35eeae61f
Tejaswini B K <124483500+9964651847@users.noreply.github.com>
2023-03-06 09:59:55 +0530

    Update codevs.py

--- codevs.py
+++ codevs.py
@@ -8,7 +8,7 @@ import os
 import smtplib
 import requests
 from bs4 import BeautifulSoup
-import win32com.client as wincl
+#import win32com.client as wincl
 
 from urllib.request import urlopen
 
@@ -125,19 +125,9 @@ if __name__ == "__main__":
                 print('The temperature in Bengaluru is', temperature, 'degrees Celsius with', description)
                 z = x["weather"]
                 weather_description = z[0]["description"]
-                #print(" Temperature (in kelvin unit) = " +str(temperature)+"\n atmospheric pressure (in hPa unit) ="+str(current_pressure) +"\n humidity (in percentage) = " +str(current_humidiy) +"\n description = " +str(weather_description)) 
+                print(" Temperature (in kelvin unit) = " +str(temperature)+"\n atmospheric pressure (in hPa unit) ="+str(current_pressure) +"\n humidity (in percentage) = " +str(current_humidiy) +"\n description = " +str(weather_description)) 
             else:
                 speak(" City Not Found ")
-
-            """if response.status_code == 200:
-            # Extract the JSON response data
-                 data = response.json()
-                 temperature = data["main"]["temp"]        # Access the desired information from the JSON data
-                 description = data['weather'][0]['description']
-                 print('The temperature in Bengaluru is', temperature, 'degrees Celsius with', description)
-            else:
-                # Handle the error
-                print('Error: HTTP status code', response.status_code)"""
             
         elif 'time' in query:
             strTime = datetime.datetime.now().strftime("%H:%M:%S")    
@@ -165,113 +155,5 @@ if __name__ == "__main__":
                 speak("Email has been sent!")
             except Exception as e:
                 print(e)
-                speak("Sorry my friend harry. I am not able to send this email")
+                speak("Sorry. I am not able to send this email")
 
-"""
-if response.status_code == 200:
-    # Process the JSON response
-    data = response.json()
-    temperature = data['main']['temp']
-    description = data['weather'][0]['description']
-    print('The temperature in Bengaluru is', temperature, 'degrees Celsius with', description)
-else:
-    # Handle the error
-    print('Error: HTTP status code', response.status_code)
-
-import requests
-
-# Define the API endpoint and parameters
-url = "http://api.openweathermap.org/data/2.5/weather"
-params = {"q": "Bengaluru", "appid": "your_api_key"}
-
-# Send a GET request to the API and store the response
-response = requests.get(url, params=params)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Extract the JSON response data
-    data = response.json()
-
-    # Access the desired information from the JSON data
-    temperature = data["main"]["temp"]
-
-
-    try:
-                jsonObj = urlopen('''https://newsapi.org / v1 / articles?source = the-times-of-india&sortBy = top&apiKey =\\times of India Api key\\''')
-                #jsonObj = request.get(urlopen("https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwj5lvy-3L_9AhX8kmYCHSuECOIYABABGgJzbQ&ase=2&ohost=www.google.com&cid=CAESbeD2W2ifNJn_AIedkI9BiHdRNEIekx-iZIV9taNJPJllB8SYclOqv9UBdRq_gIVitpdJmSG_qi9b59Grcr_tzi3OTbceJ6YQixtJkthf1bWhmw0QIFmfNu3tBrF0xPc0-tEPAvm9mAZl3JmN7Is&sig=AOD64_3PqGICchvHDe-_hBQBzwihtgxUPQ&q&nis=4&adurl&ved=2ahUKEwjl0_W-3L_9AhXN4DgGHSjUAIIQ0Qx6BAgKEAE"))
-                data = json.load(jsonObj)
-                i = 1
-                speak('here are some top news from the times of india')
-                print('''=============== TIMES OF INDIA ============'''+ '\n')
-                for item in data['articles']: 
-                    print(str(i) + '. ' + item['title'] + '\n')
-                    print(item['description'] + '\n')
-                    speak(str(i) + '. ' + item['title'] + '\n')
-                    i += 1
-            except Exception as e: 
-                print(str(e))
-
-
-
-
-if response.status_code == 200:
-            # Extract the JSON response data
-                 data = response.json()
-                 temperature = data["main"]["temp"]        # Access the desired information from the JSON data
-                 description = data['weather'][0]['description']
-                 print('The temperature in Bengaluru is', temperature, 'degrees Celsius with', description)
-            else:
-                # Handle the error
-                print('Error: HTTP status code', response.status_code)
-"""
-
-
-#jsonObj = request.get(urlopen("https://www.googleadservices.com/pagead/aclk?sa=L&ai=DChcSEwj5lvy-3L_9AhX8kmYCHSuECOIYABABGgJzbQ&ase=2&ohost=www.google.com&cid=CAESbeD2W2ifNJn_AIedkI9BiHdRNEIekx-iZIV9taNJPJllB8SYclOqv9UBdRq_gIVitpdJmSG_qi9b59Grcr_tzi3OTbceJ6YQixtJkthf1bWhmw0QIFmfNu3tBrF0xPc0-tEPAvm9mAZl3JmN7Is&sig=AOD64_3PqGICchvHDe-_hBQBzwihtgxUPQ&q&nis=4&adurl&ved=2ahUKEwjl0_W-3L_9AhXN4DgGHSjUAIIQ0Qx6BAgKEAE"))
-
-"""
-elif 'play music' in query:
-            music_dir = 'D:\\Non Critical\\songs\\Favorite Songs2'
-            songs = os.listdir(music_dir)
-            print(songs)    
-            os.startfile(os.path.join(music_dir, songs[0]))
-
-import requests
-
-# Define the API endpoint and parameters
-url = "http://api.openweathermap.org/data/2.5/weather"
-params = {"q": "Bengaluru", "appid": "your_api_key"}
-
-# Send a GET request to the API and store the response
-response = requests.get(url, params=params)
-
-# Check if the request was successful
-if response.status_code == 200:
-    # Extract the JSON response data
-    data = response.json()
-
-    # Access the desired information from the JSON data
-    temperature = data["main"]["temp"]
-"""
-
-#base_url = api_key.request('GET', 'http://api.openweathermap.org / data / 2.5 / weather?')
-            #base_url = "https://openweathermap.org/api"
-            #r = http.request('GET', 'http://google.com/')
-
-"""
-import requests
-
-# Set up the API endpoint and parameters
-url = 'https://api.openweathermap.org/data/2.5/weather'
-params = {'q': 'Bengaluru', 'appid': 'your_api_key'}
-
-# Send an HTTP GET request to the API endpoint
-response = requests.get(url, params=params)
-
-# Check the status code of the response
-if response.status_code == 200:
-    # Process the JSON response
-    data = response.json()
-    temperature = data['main']['temp']
-    description = data['weather'][0]['description']
-    print('The tem
-"""
